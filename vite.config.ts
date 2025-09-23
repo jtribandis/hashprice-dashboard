095920b195e1d6dda914c997828319dc8c1b0cdb
Jason J Tribandis <165801980+jtribandis@users.noreply.github.com>
2025-09-23 17:17:22 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hashprice-dashboard/', // <-- EXACT GitHub repo name, with slashes
+  base: '/hashprice-dashboard/v1/',  // <— bump once to v1
   resolve: { alias: { '@': '/src' } }
 })
-
-
